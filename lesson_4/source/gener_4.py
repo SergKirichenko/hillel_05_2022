26a1283eed46b75d6282ac86dc7b93deac733c6c
Serg Kirichenko <ss86kirichenko@gmail.com>
2022-06-08 21:53:42 +0300

    up gen4

--- lesson_4/source/gener_4.py
+++ lesson_4/source/gener_4.py
@@ -1,6 +1,6 @@
 from typing import Generator
 
-FILENAME = "./lesson_4_iter_gener_functions/generators/rockyou.txt"
+FILENAME = "./lesson_4/generators/rockyou.txt"
 SEARCH_KEYWORD = "admin"
 
 
@@ -12,18 +12,18 @@ def read_lines_find_admin() -> list:
     #             results.append(word)
 
     # return results
-    with open(FILENAME, encoding="utf-8") as file:
+    with open(FILENAME, "r", encoding="utf-8") as file:
         return [word for word in file.readlines() if SEARCH_KEYWORD in word]
 
 
 def bad_generator() -> Generator:
-    with open(FILENAME, encoding="utf-8") as file:
+    with open(FILENAME, "r", encoding="utf-8") as file:
         for line in file.readlines():
             yield line
 
 
 def read_lines_find_admin_generator() -> Generator:
-    with open(FILENAME, encoding="utf-8") as file:
+    with open(FILENAME, "r", encoding="utf-8") as file:
         while True:
             line = file.readline()
             if not line:
