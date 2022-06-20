96392d7087459ac8262d844f3e1daf1b60655d54
Serg Kirichenko <ss86kirichenko@gmail.com>
2022-06-20 19:45:38 +0300

    homework 4 mod

--- lesson_6/homework/homework-4.2.py
+++ lesson_6/homework/homework-4.2.py
@@ -18,3 +18,22 @@ def get_user(name: str, age: int):
 
 if __name__ == "__main__":
     print(get_user("bob", 33), get_user("Alice", 15), sep="\n")
+
+###############################################################################################
+# Homework 4 - Decorators Part 2.1
+
+
+def mask_info(func):
+    def wrapper(name: str, age: int):
+        return {"name": ("*" * len(name)), "age": age}
+
+    return wrapper
+
+
+@mask_info
+def get_user(name: str, age: int):
+    return {"name": name, "age": age}
+
+
+if __name__ == "__main__":
+    print(get_user("bob", 33), get_user("Alice", 15), sep="\n")
